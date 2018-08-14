d10b97f53fc7d08c15bee77d94f581d265b69a5d
Lindsay Sack <lindsay.sack@gmail.com>
2018-08-14 17:47:53 -0400

    Set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+    entry: './app/main', // the starting point for our program
+    output: {
+      path: __dirname + '/public', // the absolute path for the directory where we want the output to be placed
+      filename: 'bundle.js' // the name of the file that will contain our output - we could name this whatever we want, but bundle.js is typical
+    }
+  }
